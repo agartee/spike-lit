7faff6a087cdb698c3c97e6ba3bad12c94044aa3
Adam Gartee <agartee@gmail.com>
2023-05-24 08:04:37 -0400

    initial commit

---
+++ src/MyElement.js
@@ -0,0 +1,35 @@
+import { html, css, LitElement } from 'lit';
+
+export class MyElement extends LitElement {
+  static styles = css`
+    :host {
+      display: block;
+      padding: 25px;
+      color: var(--my-element-text-color, #000);
+    }
+  `;
+
+  static properties = {
+    header: { type: String },
+    counter: { type: Number },
+    name: { type: Object }
+  };
+
+  constructor() {
+    super();
+    this.header = 'Hey there';
+    this.counter = 5;
+  }
+
+  __increment() {
+    this.counter += 1;
+  }
+
+  render() {
+    return html`
+      <h1>${this.header} Nr. ${this.counter}!</h2>
+      <p>${this.name.first}</p>
+      <button @click=${this.__increment}>increment</button>
+    `;
+  }
+}
